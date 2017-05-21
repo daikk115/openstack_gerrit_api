87f68c2aeb1b867acc022e582076e6129d001085
Dai Dang Van <daikk115@gmail.com>
2017-05-22 00:51:27 +0700

    Add write file

--- test.py
+++ test.py
@@ -2,12 +2,14 @@ import json
 import requests
 
 
-KEYWORD = 'objects'
+KEYWORD = 'six.iteritems'
 OPS_GERRIT_API = 'https://review.openstack.org'
-GET_CHANGES_API = '/changes/?q=status:open&n=100&o=CURRENT_REVISION&o=CURRENT_FILES'
+GET_CHANGES_API = '/changes/?q=status:open&n=1000&o=CURRENT_REVISION&o=CURRENT_FILES'
 
 r = requests.get("{}{}".format(OPS_GERRIT_API, GET_CHANGES_API))
 changes = json.loads(r.content.replace(")]}'", "").strip())
+
+f = open('links.txt', 'w+')
 
 for change in changes:
     number = change['_number']
@@ -15,14 +17,18 @@ for change in changes:
     revisions = change['revisions']
     current_revision_id = revisions.keys()[0]
     current_revision = revisions[current_revision_id]
+    current_revision_number = current_revision['_number']
     for name in current_revision['files']:
-        name = name.replace('/', '%2F')
         get_diff_api = "{}/changes/{}/revisions/{}/files/{}/diff".format(
-           OPS_GERRIT_API, change_id, current_revision_id, name
+           OPS_GERRIT_API, change_id, current_revision_id, name.replace('/', '%2F')
         )
         r = requests.get(get_diff_api)
         diff = r.content
         if KEYWORD in diff:
-            print("{}/#/c/{}/".format(
-                OPS_GERRIT_API, number
-            ))
+            link = "{}/#/c/{}/{}/{}".format(
+                OPS_GERRIT_API, number,
+                current_revision_number, name
+            )
+            print(link)
+            f.write("{}\n".format(link))
+f.close()
