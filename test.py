ea578f26d6a8f5da2fc9fc885fdd47e5c2daaeea
Dai Dang Van <daikk115@gmail.com>
2017-05-23 00:45:53 +0700

    Update

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-KEYWORD = 'six.iteritems'
+KEYWORD = 'unicode'
 OPS_GERRIT_API = 'https://review.openstack.org'
 GET_CHANGES_API = '/changes/?q=status:open&n=1000&o=CURRENT_REVISION&o=CURRENT_FILES'
 
@@ -22,13 +22,22 @@ for change in changes:
         get_diff_api = "{}/changes/{}/revisions/{}/files/{}/diff".format(
            OPS_GERRIT_API, change_id, current_revision_id, name.replace('/', '%2F')
         )
+        link = "{}/#/c/{}/{}/{}".format(
+            OPS_GERRIT_API, number,
+            current_revision_number, name
+        )
         r = requests.get(get_diff_api)
         diff = r.content
-        if KEYWORD in diff:
-            link = "{}/#/c/{}/{}/{}".format(
-                OPS_GERRIT_API, number,
-                current_revision_number, name
-            )
-            print(link)
-            f.write("{}\n".format(link))
+        try:
+            diff_dict = json.loads(diff.replace(")]}'", "").strip())
+        except:
+            continue
+        list_change_parts = diff_dict.get('content')
+        if isinstance(list_change_parts, list):
+            for part in list_change_parts:
+                # b: new code, a: code deleted, ab: not change
+                new = part.get('b')
+                if new and KEYWORD in new:
+                    print(link)
+                    f.write("{}\n".format(link))
 f.close()
